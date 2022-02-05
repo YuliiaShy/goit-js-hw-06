66207e9159b42963b54adf7df1ef778515db101f
Yuliia Shybyk <j7557500@gmail.com>
2022-02-05 11:09:51 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,18 +8,20 @@ const ingredients = [
 ];
 
 const ingredientsList = document.querySelector("#ingredients");
-console.log(ingredientsList); 
 
-const elements = ingredients.map(element => {
-  const itemEl = document.createElement("li");
-  itemEl.classList.add("item");
-  itemEl.textContent = element;
-  return itemEl;
-})
+const makeElements = ingredients => {
+  return ingredients.map(element => {
+    const itemEl = document.createElement("li");
+    itemEl.classList.add("item");
+    itemEl.textContent = element;
+    return itemEl;
+  });
+};
 
+const elements = makeElements(ingredients);
 ingredientsList.append(...elements);
 
-console.log(elements);
+
 
 
 
