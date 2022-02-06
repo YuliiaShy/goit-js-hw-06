54337d859128ee2af92d0e89dd437ece7cd4335f
Yuliia Shybyk <j7557500@gmail.com>
2022-02-06 19:08:23 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const buttonEl = document.querySelector('.change-color');
+
+const spanColorEl = document.querySelector('.color');
+
+const onChangeColor = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanColorEl.textContent = getRandomHexColor();
+}
+
+buttonEl.addEventListener('click', onChangeColor);
