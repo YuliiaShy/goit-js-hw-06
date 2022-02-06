b09d5cdbed2265f341aed0d6b8e4086b9309a7d6
Yuliia Shybyk <j7557500@gmail.com>
2022-02-06 16:20:42 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const loginForm = document.querySelector('.login-form');
+
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+loginForm.addEventListener('submit', onFormSubmit);
