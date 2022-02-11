66eac36125567d3fd24143c5f5f05511a17c4b50
Yuliia Shybyk <j7557500@gmail.com>
2022-02-11 17:50:58 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,10 @@
 
 const validationInput = document.querySelector('#validation-input');
 
+const DatasetLength = parseInt(document.querySelector('#validation-input').dataset.length);
+
 const onInputValid = event => {
-  if (event.currentTarget.value.length === 6) {
+  if (event.currentTarget.value.length === DatasetLength) {
     validationInput.classList.add('valid');
     validationInput.classList.remove('invalid');
   } else {
